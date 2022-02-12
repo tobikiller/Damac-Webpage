5529351de03ecae6c6505c99a4641256a58ec13d
tobikiller <edwardnguimeya@yahoo.com>
2022-02-12 14:08:44 +0100

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        greenTeal: "#7D8F3A",
+        darkCoal: "#2D2323"
+      },
+      fontFamily: {
+        'Montserrat': "Montserrat",
+        'Poppins': "poppins"
+      }
+    },
   },
   plugins: [],
 }
