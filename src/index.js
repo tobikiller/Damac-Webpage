130845d850839be64108569772e69e4f4a093336
tobikiller <edwardnguimeya@yahoo.com>
2022-02-12 18:04:03 +0100

    Change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 const btnToggle = document.getElementById("toggle-btn")
 const navBar = document.querySelector(".navbar")
+const btnColor = document.querySelectorAll(".btn-color")
+const rotateR = document.querySelector(".btn-rotateR")
+const rotateL = document.querySelector(".btn-rotateL")
+const Btnopacity = document.querySelector(".btn-opacity")
 
 btnToggle.addEventListener("click", function(){
-    navBar.classList.toggle("show")
+ 
+    btnColor.forEach(function(e){
+        if(navBar.classList.contains("show")){
+            navBar.classList.remove("show")
+            rotateR.classList.add("backR")
+         
+            rotateR.classList.toggle("backR")
+            console.log(rotateR);
+        }else{
+            navBar.classList.add("show")
+       
+            
+ 
+           
+        }
+     })
+ 
     
+
+    rotateR.classList.toggle("showR")
+    console.log(rotateR);
+    Btnopacity.classList.toggle("op")
+    rotateL.classList.toggle("showL")
 })
