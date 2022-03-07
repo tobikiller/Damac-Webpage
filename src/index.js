81fa4c6707b0a84c057d5f50b8cb9f2e54f0d4f9
tobikiller <edwardnguimeya@yahoo.com>
2022-03-07 14:44:38 +0100

    new

--- src/index.js
+++ src/index.js
@@ -73,16 +73,65 @@ function moveSlide(trackBox, currentSlide, targetslide){
     targetslide.classList.add("carousel-slide")
 }
 
-nextBtn.addEventListener("click", e=> {
-    const currentSlide = trackBox.querySelector(".carousel-slide")
-    const nextSlide = currentSlide.nextElementSibling;
-
-    moveSlide(trackBox, currentSlide , nextSlide)
-})
 
 prevBtn.addEventListener("click", e=>{
     const currentSlide = trackBox.querySelector(".carousel-slide")
     const prevSlide = currentSlide.previousElementSibling;
-
+    const currentDot = indicator.querySelector(".current-slide")
+    const prevDot = currentDot.previousElementSibling
+    const prevIndex = slides.findIndex(slide => slide === prevSlide)
+  
     moveSlide(trackBox, currentSlide , prevSlide)
+    updateDots(currentDot,prevDot)
+    hiddenArrows(slides, prevBtn, nextBtn, prevIndex)
+
+})
+
+
+nextBtn.addEventListener("click", e=> {
+    const currentSlide = trackBox.querySelector(".carousel-slide")
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = indicator.querySelector(".current-slide")
+    const nextDot = currentDot.nextElementSibling
+    const nextIndex = slides.findIndex(slide => slide === nextSlide)
+    console.log(nextIndex);
+  
+    moveSlide(trackBox, currentSlide , nextSlide)
+    updateDots(currentDot,nextDot)
+    hiddenArrows(slides, prevBtn, nextBtn, nextIndex)
+})
+
+
+function hiddenArrows(slides, prevBtn, nextBtn, targetIndex){
+    if(targetIndex === 0){
+        prevBtn.classList.add("hidden-arrow")
+        nextBtn.classList.remove("hidden-arrow")
+    }else if(targetIndex === slides.length -1){
+        prevBtn.classList.remove("hidden-arrow")
+        nextBtn.classList.add("hidden-arrow")
+    }else{
+        prevBtn.classList.remove("hidden-arrow")
+        nextBtn.classList.remove("hidden-arrow")
+    }
+}
+
+
+// indicator
+function updateDots(currentDot,targetDot){
+    currentDot.classList.remove("current-slide")
+    targetDot.classList.add("current-slide")
+}
+
+indicator.addEventListener("click", e=>{
+    const targetDot = e.target.closest('button')
+   if(!targetDot) return
+    const currentSlide = trackBox.querySelector(".carousel-slide")
+    const currentDot = indicator.querySelector(".current-slide")
+    
+    const targetIndex = dots.findIndex(dots => dots === targetDot)
+    const targetSlide = slides[targetIndex]
+
+    moveSlide(trackBox, currentSlide , targetSlide)
+    updateDots(currentDot,targetDot)
+    hiddenArrows(slides, prevBtn, nextBtn, targetIndex )
 })
